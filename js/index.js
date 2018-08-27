13283aa66d495a266c816975be72d254ce629e42
Govind Menon <govindmenon1995@yahoo.co.in>
2018-08-27 23:42:46 +0530

    bug fixes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
 var homeNavHeight = document.getElementById('nav-wrapper').offsetHeight;
 document.getElementById('carousel-example').style.top = homeNavHeight;
-$(function(){
-	$("#login-modal").load("/get-login-modal"); 
-});
 
 
